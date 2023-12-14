c8bb78df211719f874cd70085141a6a453108b05
Mauricio Ayllón Varela <mauri051928@gmail.com>
2023-12-14 17:52:59 -0500

    invalid actualization

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,14 @@
 import Navbar1 from "./components/Navbar";
 import HomePage from "./peges/HomePage";
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
-  return (
-    
-     <>
-     <Navbar1 />
-     <HomePage />
-     
-     </>
-  )
+  return (<>
+    <Navbar1 />
+    <HomePage />
+    <Routes></Routes> 
+  </>)
 }
 
 export default App
