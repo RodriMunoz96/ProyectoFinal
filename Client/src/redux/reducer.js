88accf48229f38cb0a65bcb1cdd6c6e731a8b4e4
RodriMunoz96 <rodrigo.gaston.munoz@gmail.com>
2023-12-15 22:10:26 -0300

    se arregló el problema con el conflicto de archivos redux

--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -24,77 +24,18 @@ const initialState = {
 };
 
 // Define el reducer que gestionará las acciones y actualizará el estado
-const studentReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case CREATE_STUDENT:
-      // Agrega un nuevo estudiante a la lista
+const rootReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case GET_ALL_USERS:
       return {
         ...state,
-        students: [...state.students, action.payload],
+        allUsers: payload,
       };
 
-    case GET_ALL_STUDENTS:
-      // Actualiza la lista de estudiantes con los datos obtenidos de la acción
+    case GET_USER:
       return {
         ...state,
-        students: action.payload,
-      };
-
-    case GET_STUDENT_BY_ID:
-      // Actualiza los detalles del estudiante con los datos obtenidos de la acción
-      return {
-        ...state,
-        detailStudent: action.payload,
-      };
-
-    // Otras acciones para actualizar, eliminar, buscar por nombre, limpiar detalles y filtros...
-    
-
-    case CLEAR_STUDENT_DETAIL:
-      // Borra los detalles del estudiante (se utiliza para limpiar los detalles)
-      return {
-        ...state,
-        detailStudent: {},
-      };
-
-    case BY_GRADE:
-      // Filtra la lista de estudiantes por grado
-      let filteredByGrade = state.students.filter(
-        (student) => student.grade === action.payload
-      );
-      return {
-        ...state,
-        filteredStudents: filteredByGrade,
-      };
-
-    case BY_PARENT:
-      // Filtra la lista de estudiantes por padre
-      let filteredByParent = state.students.filter(
-        (student) => student.parentId === action.payload
-      );
-      return {
-        ...state,
-        filteredStudents: filteredByParent,
-      };
-
-    case BY_TEACHER:
-      // Filtra la lista de estudiantes por profesor
-      let filteredByTeacher = state.students.filter(
-        (student) => student.teacherId === action.payload
-      );
-      return {
-        ...state,
-        filteredStudents: filteredByTeacher,
-      };
-
-    case BY_SUBJECT:
-      // Filtra la lista de estudiantes por asignatura
-      let filteredBySubject = state.students.filter(
-        (student) => student.subject === action.payload
-      );
-      return {
-        ...state,
-        filteredStudents: filteredBySubject,
+        user: payload,
       };
 
     case UPDATE_USER:
@@ -160,9 +101,8 @@ const studentReducer = (state = initialState, action) => {
       };
 
     default:
-      // Devuelve el estado sin cambios si la acción no es reconocida
-      return { ...state };
+      return state;
   }
 };
 
-export default studentReducer; // Exporta el reducer para su uso en la aplicación
+export default rootReducer;
--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -1,15 +1,9 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './reducer'; // Importa el reducer que define cómo se actualiza el estado
-import thunkMiddleware from 'redux-thunk'; // Importa el middleware de Redux Thunk para manejar acciones asincrónicas
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducer";
+import thunk from "redux-thunk";
 
-// ComposeEnhancer permite habilitar la extensión de Redux DevTools en el navegador
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// Crea la tienda Redux utilizando el rootReducer y aplicando el middleware Redux Thunk
-const store = createStore(
-  rootReducer, // El reducer que maneja el estado de la aplicación
-  composeEnhancer(applyMiddleware(thunkMiddleware)) // Aplica Redux Thunk como middleware
-);
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
-export default store; // Exporta la tienda para su uso en la aplicación
-
+export default store;
