dd8fd00968e5b90dd1d028893e46a6723aca540a
Julian Guzman <jhguzmans@unal.edu.co>
2023-12-16 10:47:03 -0500

    rutas del nav

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar1() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">DataCole</Navbar.Brand>
+        <Navbar.Brand href="home">DataCole</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
