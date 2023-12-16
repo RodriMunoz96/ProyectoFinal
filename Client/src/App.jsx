24dee3cccd8f4e8220ccc3581e3d3ceeab32e6c4
Julian Guzman <jhguzmans@unal.edu.co>
2023-12-16 10:41:51 -0500

    rutas organizadas, falta crear admin

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,9 +5,12 @@ import Footer from "./components/Footer";
 import Login from "./components/login";
 import FormParent from "./components/Forms/FormParent/FormParent";
 import Admin from "./pages/Admin";
-
-
-
+import QuienesSomos from "./peges/QuienesSomos";
+import QueTeOfrecemos from "./components/QueTeOfrecemos";
+import PorQueElegir from "./components/PorqueElegir";
+import NotFound from "./peges/NotFound";
+import NuestroEquipo from "./peges/NuestroEquipo";
+import AddUserForm from "./components/AddUserForm";
 function App() {
   return (
     <>
@@ -15,8 +18,14 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/login" element={<Login />} />
-        <Route exact path="/formParent" element={<FormParent/>}/>
+        <Route exact path="/formParent" element={<FormParent />} />
         <Route exact path="/admin" element={<Admin />} />
+        <Route exact path="/QuienesSomos" element={<QuienesSomos />} />
+        <Route exact path="/oferta" element={<QueTeOfrecemos />} />
+        <Route exact path="/porqueelegir" element={<PorQueElegir />} />
+        <Route exact path="/nuestroequipo" element={<NuestroEquipo />} />
+        <Route exact path="/addUser" element={<AddUserForm />} />
+        <Route exact path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
