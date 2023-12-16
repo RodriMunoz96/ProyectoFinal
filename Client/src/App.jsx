014ec8ab07e19c3e8cce78b97f3720a6d2815170
Julian Guzman <jhguzmans@unal.edu.co>
2023-12-16 11:08:39 -0500

    rutasDelNav2

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,16 @@
 import Navbar1 from "./components/Navbar";
-import HomePage from "./pages/HomePage";
+import HomePage from "./peges/HomePage";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Login from "./components/login";
 import FormParent from "./components/Forms/FormParent/FormParent";
-import Admin from "./pages/Admin";
 import QuienesSomos from "./peges/QuienesSomos";
 import QueTeOfrecemos from "./components/QueTeOfrecemos";
 import PorQueElegir from "./components/PorqueElegir";
 import NotFound from "./peges/NotFound";
 import NuestroEquipo from "./peges/NuestroEquipo";
 import AddUserForm from "./components/AddUserForm";
+import Contacto from "./peges/Contacto";
 function App() {
   return (
     <>
@@ -19,12 +19,13 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/formParent" element={<FormParent />} />
-        <Route exact path="/admin" element={<Admin />} />
         <Route exact path="/QuienesSomos" element={<QuienesSomos />} />
         <Route exact path="/oferta" element={<QueTeOfrecemos />} />
         <Route exact path="/porqueelegir" element={<PorQueElegir />} />
         <Route exact path="/nuestroequipo" element={<NuestroEquipo />} />
         <Route exact path="/addUser" element={<AddUserForm />} />
+        <Route exact path="/contacto" element={<Contacto />} />
+
         <Route exact path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
--- Client/src/components/QueTeOfrecemos.jsx
+++ Client/src/components/QueTeOfrecemos.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
+//import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
 
 const QueTeOfrecemos = () => {
-    return (
-        <section className="large-section-padding stunning-header-bg9">
-            <Container>
-                <Row>
-                    <Col lg={8} md={12} sm={12} xs={12} className="align-center mb-5 ml-auto mr-auto">
-                        <h2 className="text-black">¿Qué te ofrecemos?</h2>
-                        <p className="fs-18 fw-medium text-black">Una plataforma, múltiples funciones.</p>
-                    </Col>
-                </Row>
-                
-            </Container>
-        </section>
-    );
+  return (
+    <section className="large-section-padding stunning-header-bg9">
+      <Container>
+        <Row>
+          <Col
+            lg={8}
+            md={12}
+            sm={12}
+            xs={12}
+            className="align-center mb-5 ml-auto mr-auto"
+          >
+            <h2 className="text-black">¿Qué te ofrecemos?</h2>
+            <p className="fs-18 fw-medium text-black">
+              Una plataforma, múltiples funciones.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default QueTeOfrecemos;
