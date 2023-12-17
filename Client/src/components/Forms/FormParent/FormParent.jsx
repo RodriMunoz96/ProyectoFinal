1a7920486c90c6663dd7e309bbe2f79210e20a2e
Mauricio Ayllón Varela <mauri051928@gmail.com>
2023-12-17 18:30:13 -0500

    Validaciones agregadas al formulario de creación de padre

--- Client/src/components/Forms/FormParent/FormParent.jsx
+++ Client/src/components/Forms/FormParent/FormParent.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import style from './formParent.module.css'
 import { createParent } from '../../../redux/actions/actions-parents.js';
+import { validation } from './validation.js';
 
 const FormParent = () => {
     const dispatch = useDispatch()
+    const [ errors, setErrors ] = useState({})
     const [ newParent, setNewParent ] = useState({
         idDoc: "", //image
         name: "",
@@ -18,22 +20,30 @@ const FormParent = () => {
         contactCellphone: "",
         email: "",
         tutor: true,
+        // userId: "19302f1a-93d1-4473-a151-c4df39df503c"
     })
 
     const handleChange = (e) => {
         const { name, value } = e.target
         
+        setErrors(validation({
+            ...newParent,
+            [name]: value
+        }))
         setNewParent({
             ...newParent,
             [name]: value
         })
+        console.log(errors);
     }
 
     const onSubmit = (e) => {
         e.preventDefault()
 
-        dispatch(createParent({...newParent}))
+        // dispatch(createParent({...newParent}))
+        console.log(newParent);
 
+        setErrors({})
         setNewParent({
             idDoc: "", //image
             name: "",
@@ -46,7 +56,8 @@ const FormParent = () => {
             jobTelephone: "",
             contactCellphone: "",
             email: "",
-            tutor: true
+            tutor: true,
+            // userId: "19302f1a-93d1-4473-a151-c4df39df503c"
         })
     }
 
@@ -57,40 +68,112 @@ const FormParent = () => {
                     <h1>Información general del Apoderado</h1>
                 </nav>
                 <div className={style.container_label_inputs}>
-                    <label htmlFor="idDoc">Documento de identidad:</label>
-                    <input value={newParent.idDoc} onChange={handleChange} type="text" name='idDoc'/>
+                    <div className={style.first_container}>
+                        <div className={style.first_first_container}>
+                            <div className={style.container_image}>+</div>
+                        </div>
+                        <div className={style.second_first_container}>
+                            <div className={style.second_first_container_first_line}>
+                                <div className={style.container_name}>
+                                    <label htmlFor="name">Nombre:</label>
+                                    <input value={newParent.name} onChange={handleChange} type="text" name='name'/>
+                                    <br />
+                                    <p>{errors.name ? errors.name: null}</p>
+                                </div>
+                                <div className={style.container_lastName}>
+                                    <label htmlFor="lastName">Apellido:</label>
+                                    <input value={newParent.lastName} onChange={handleChange} type="text" name='lastName'/>
+                                    <br />
+                                    <p>{errors.lastName ? errors.lastName: null}</p>
+                                </div>
+                            </div>
+                            <div className={style.second_first_container_second_line}>
+                                <div className={style.container_idDoc}>
+                                    <label htmlFor="idDoc">Documento de identidad:</label>
+                                    <input value={newParent.idDoc} onChange={handleChange} type="text" name='idDoc'/>
+                                    <br />
+                                    <p>{errors.idDoc ? errors.idDoc: null}</p>
+                                </div>
+                                <div className={style.container_email}>
+                                    <label htmlFor="email">Email:</label>
+                                    <input value={newParent.email} onChange={handleChange} type="email" name='email'/>
+                                    <br />
+                                    <p>{errors.email ? errors.email: null}</p>
+                                </div>
+                            </div>
+                            <div className={style.second_first_container_third_line}>
+                                <div className={style.container_educationLevel}>
+                                    <label htmlFor="educationLevel">Nivel educacional: (grado)</label>
+                                    <input value={newParent.educationLevel} onChange={handleChange} type="text" name='educationLevel'/>
+                                    <br />
+                                    <p>{errors.educationLevel ? errors.educationLevel: null}</p>
+                                </div>
+                                <div className={style.container_profession}>
+                                    <label htmlFor="profession">Profesión:</label>
+                                    <input value={newParent.profession} onChange={handleChange} type="text" name='profession'/>
+                                    <br />
+                                    <p>{errors.profession ? errors.profession: null}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.second_container}>
+                        <div className={style.first_second_container}>
+                            <div className={style.container_telephone}>
+                                <label htmlFor="telephone">Télefono del hogar:</label>
+                                <input value={newParent.telephone} onChange={handleChange} type="text" name='telephone'/>
+                                <br />
+                                <p>{errors.telephone ? errors.telephone: null}</p>
+                            </div>
+                            <div className={style.container_jobTelephone}>
+                                <label htmlFor="jobTelephone">Télefono del trabajo:</label>
+                                <input value={newParent.jobTelephone} onChange={handleChange} type="text" name='jobTelephone'/>
+                                <br />
+                                <p>{errors.jobTelephone ? errors.jobTelephone: null}</p>
+                            </div>
+                            <div className={style.container_contactCellphone}>
+                                <label htmlFor="contactCellphone">Celular:</label>
+                                <input value={newParent.contactCellphone} onChange={handleChange} type="text" name='contactCellphone'/>
+                                <br />
+                                <p>{errors.contactCellphone ? errors.contactCellphone: null}</p>
+                            </div>
+                        </div>
+                        <div className={style.second_second_container}>
+                            <div className={style.container_address}>
+                                <label htmlFor="address">Dirección del Hogar:</label>
+                                <input value={newParent.address} onChange={handleChange} type="text" name='address'/>
+                                <br />
+                                <p>{errors.address ? errors.address: null}</p>
+                            </div>
+                            <div className={style.container_jobAddress}>
+                                <label htmlFor="jobAddress">Dirección del Trabajo: </label>
+                                <input value={newParent.jobAddress} onChange={handleChange} type="text" name='jobAddress'/>
+                                <br />
+                                <p>{errors.jobAddress ? errors.jobAddress: null}</p>
+                            </div>
+                        </div>
+                    </div> 
 
-                    <label htmlFor="name">Nombre:</label>
-                    <input value={newParent.name} onChange={handleChange} type="text" name='name'/>
-
-                    <label htmlFor="lastName">Apellido:</label>
-                    <input value={newParent.lastName} onChange={handleChange} type="text" name='lastName'/>
-
-                    <label htmlFor="educationLevel">Nivel educacional:</label>
-                    <input value={newParent.educationLevel} onChange={handleChange} type="text" name='educationLevel'/>
-
-                    <label htmlFor="profession">Profesión:</label>
-                    <input value={newParent.profession} onChange={handleChange} type="text" name='profession'/>
-
-                    <label htmlFor="address">Dirección del Hogar:</label>
-                    <input value={newParent.address} onChange={handleChange} type="text" name='address'/>
-
-                    <label htmlFor="jobAddress">Dirección del Trabajo: </label>
-                    <input value={newParent.jobAddress} onChange={handleChange} type="text" name='jobAddress'/>
-
-                    <label htmlFor="telephone">Télefono del hogar:</label>
-                    <input value={newParent.telephone} onChange={handleChange} type="text" name='telephone'/>
-
-                    <label htmlFor="jobTelephone">Télefono del trabajo:</label>
-                    <input value={newParent.jobTelephone} onChange={handleChange} type="text" name='jobTelephone'/>
-
-                    <label htmlFor="contactCellphone">Celular:</label>
-                    <input value={newParent.contactCellphone} onChange={handleChange} type="text" name='contactCellphone'/>
-
-                    <label htmlFor="email">Email:</label>
-                    <input value={newParent.email} onChange={handleChange} type="email" name='email'/>
-                </div>
-                <button type='submit' className={style.submit_button}>Enviar</button>
+                </div>              
+                <button 
+                    type='submit' 
+                    className={style.submit_button}
+                    disabled={Object.keys(errors).length > 0 || 
+                        !newParent.idDoc ||
+                        !newParent.name ||
+                        !newParent.lastName ||
+                        !newParent.educationLevel ||
+                        !newParent.profession ||
+                        !newParent.address ||
+                        !newParent.jobAddress || 
+                        !newParent.telephone || 
+                        !newParent.jobTelephone ||
+                        !newParent.contactCellphone ||
+                        !newParent.email  
+                        ? true : false}
+                >
+                    Enviar
+                </button>
             </form>
         </div>
     </>);
---
+++ Client/src/components/Forms/FormParent/validation.js
@@ -0,0 +1,136 @@
+export const validation = (newParent) => {
+    const error = {}
+
+    /* REGEX */
+    // const idDocRegex = 1;
+    const nameRegex = /^[a-zA-Z0-9áéíóúüÁÉÍÓÚÜñÑäëïöüÄËÏÖÜ]+$/;
+    const educationLevelRegex = /^[a-zA-Z]+$/;
+    const professionRegex = /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ]+(?:[ ]?[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ]+)*$/
+    const addresesRegex = /^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ]+(?:[ ]?[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ.]+)*$/;
+    const numberRegex = /^[0-9]+$/
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+    /* ID DOC */
+    if(!numberRegex.test(newParent.idDoc)){
+        error.idDoc = "El documento de identidad debe ser numérico."
+    }
+    if(newParent.idDoc.length > 14 || newParent.idDoc.length < 7){
+        error.idDoc = "El DNI debe contener entre 7 y 14 dígitos."
+    }
+    if(newParent.idDoc.length === 0){
+        error.idDoc = "Complete este campo con su DNI";
+    }
+
+    /* NAME */
+    if(!nameRegex.test(newParent.name)){
+        error.name = "El nombre contiene carácteres inválidos."
+    }
+    if(newParent.name.length > 45){
+        error.name = 'El nombre no puede tener más de 45 carácteres.'
+    }
+    if(newParent.name.length === 0){
+        error.name = "Ingrese su nombre."
+    }
+
+    /* LASTNAME */
+    if(!nameRegex.test(newParent.lastName)){
+        error.lastName = "El apellido contiene cáracteres inválidos."
+    }
+    if(newParent.lastName.length > 36){
+        error.lastName = "El apellido no puede tener más de 36 carácteres."
+    }
+    if(newParent.lastName.length === 0){
+        error.lastName = "Ingrese su apellido.";
+    }
+
+    /* EDUCATION LEVEL */
+    if(!educationLevelRegex.test(newParent.educationLevel)){
+        error.educationLevel = "El nivel educativo contiene carácteres inválidos."
+    }
+    if(newParent.educationLevel.length > 15){
+        error.educationLevel = "El nivel educativo no debe superar los 15 carácteres."
+    }
+    if(newParent.educationLevel.length === 0){
+        error.educationLevel = "Indique su máximo nivel educativo."
+    }
+
+    /* PROFESSION */
+    if(!professionRegex.test(newParent.profession)){
+        error.profession = "La profesión solo admite letras y espacios en blanco."
+    }
+    if(newParent.profession.length > 30){
+        error.profession = "La profesión no debe contener mas de 30 carácteres."
+    }
+    if(newParent.profession.length ===0){
+        error.profession = "Ingrese su profesión u oficio."
+    }
+
+    /* ADDRESS */
+    if(!addresesRegex.test(newParent.address)){
+        error.address = "La dirección contiene carácteres inválidos."
+    }
+    if(newParent.address.length > 60){
+        error.address = "La dirección no debe exceder de 60 carácteres."
+    }
+    if(newParent.address.length === 0) {
+        error.address = "Ingrese la dirección de su hogar."
+    }
+
+    /* JOB ADDRESS */
+    if(!addresesRegex.test(newParent.jobAddress)){
+        error.jobAddress = "La dirección contiene carácteres inválidos."
+    }
+    if(newParent.jobAddress.length > 60){
+        error.jobAddress = "La dirección no debe exceder de 60 carácteres."
+    }
+    if(newParent.jobAddress.length === 0){
+        error.jobAddress = "Ingrese la dirección de su trabajo u oficio."
+    }
+
+    /* TELEPHONE */
+    if(!numberRegex.test(newParent.telephone)){
+        error.telephone = "El teléfono debe ser numérico."
+    }
+    if(newParent.telephone.length > 14 || newParent.telephone.length < 8){
+        error.telephone = "El número de teléfono debe contener entre 8 y 14 dígitos."
+    }
+    if(newParent.telephone.length === 0){
+        error.telephone = "Ingrese el teléfono fijo."
+    }
+
+    /* JOB TELEPHONE */
+    if(!numberRegex.test(newParent.jobTelephone)){
+        error.jobTelephone = "El Teléfono debe ser numérico."
+    }
+    if(newParent.jobTelephone.length > 15 || newParent.jobTelephone.length < 8){
+        error.jobTelephone = "El número del teléfono laboral debe contener entre 8 y 15 dígitos."
+    }
+    if(newParent.jobTelephone.length === 0){
+        error.jobTelephone = "Ingrese el teléfono del lugar donde realiza sus labores u oficio."
+    }
+
+    /* CONTACT CELL PHONE */
+    if(!numberRegex.test(newParent.contactCellphone)){
+        error.contactCellphone = "El número del celular debe ser númerico."
+    }
+    if(newParent.contactCellphone.length > 13 || newParent.contactCellphone.length < 8){
+        error.contactCellphone = "El número de celular debe contener entre 8 y 13 dígitos."
+    }
+    if(newParent.contactCellphone.length === 0){
+        error.contactCellphone = "Ingrese un número celular para contacto."
+    }
+
+    /* EMAIL */
+    if(!emailRegex.test(newParent.email)){
+        error.email = "Correo electrónico con formato incorrecto."
+    }
+    if(newParent.email.length > 50){
+        error.email = "El correo electrónico supera los 50 carácteres permitidos."
+    }
+    if(newParent.email.length === 0){
+        error.email = "Ingrese un correo electrónico."
+    }
+
+    return error
+}
