e276fd82dbca72364753c235e6db3beeae806907
RodriMunoz96 <rodrigo.gaston.munoz@gmail.com>
2023-12-20 03:43:42 -0300

    formulario Login acomodado, funcionalidades en fase de prueba

--- Client/src/components/login.jsx
+++ Client/src/components/login.jsx
@@ -1,37 +1,71 @@
+import { useState } from "react";
 import { Form, Button, Container } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { loginUser } from "../redux/actions/actions-login";
 
-function Login() {
+const Login = () => {
+  const dispatch = useDispatch();
+
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    dispatch(loginUser(loginData));
+    console.log("iniciando sesión", loginData);
+  };
+
   return (
     <Container
       className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}
     >
-      <Form className="p-4 border rounded">
-        <h3 className="text-center mb-4">Sign In</h3>
+      <Form className="p-4 border rounded" onSubmit={handleSubmit}>
+        <h3 className="text-center mb-4">Inicie sesión</h3>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Label>Correo electrónico</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Ingrese su correo"
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password" />
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Ingrese su contraseña"
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Remember me" />
+          <Form.Check type="checkbox" label="Recuérdame" />
         </Form.Group>
 
         <div className="d-grid">
           <Button variant="primary" type="submit">
-            Submit
+            Iniciar sesión
           </Button>
         </div>
 
         <p className="forgot-password text-right mt-3">
-          Forgot <a href="#">password?</a>
+          Olvidó su <a href="#">contraseña?</a>
         </p>
         <p className="forgot-password text-right mt-3">No está registrado?</p>
         <div className="d-grid">
@@ -42,6 +76,6 @@ function Login() {
       </Form>
     </Container>
   );
-}
+};
 
 export default Login;
