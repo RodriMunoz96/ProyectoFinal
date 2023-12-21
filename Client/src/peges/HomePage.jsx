ce0f1cd84e0446423ec51353622ab4014246f903
Julian Guzman <jhguzmans@unal.edu.co>
2023-12-20 23:08:10 -0500

    Cloudinary terminado, por fin

--- Client/src/peges/HomePage.jsx
+++ Client/src/peges/HomePage.jsx
@@ -1,12 +1,10 @@
 import Carrousel from "../components/Carrousel";
 import PorqueElegir from "../components/PorqueElegir";
 import QueTeOfrecemos from "../components/QueTeOfrecemos";
-import FormStudent from "../components/Forms/FormStudent/formStudent";
 
 const HomePage = () => {
   return (
     <>
-      <FormStudent />
       <Carrousel />
       <PorqueElegir />
       <QueTeOfrecemos />
