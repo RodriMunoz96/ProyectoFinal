3c90ed556bfd1ef44b91981aa7047fd292cb3948
RodriMunoz96 <rodrigo.gaston.munoz@gmail.com>
2023-12-27 19:04:34 -0300

    Lograda la autenticación del token

--- Client/src/redux/actions/actionAuth.js
+++ Client/src/redux/actions/actionAuth.js
@@ -8,18 +8,18 @@ export const authenticateUser = () => async () => {
     return { success: false };
   }
 
-  const headers = {
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
-  };
+  // const headers = {
+  //   Authorization: `Bearer ${token}`,
+  //   "Content-Type": "application/json",
+  // };
   try {
     console.log("Token before request:", token);
-    const response = await axios.get(
-      "http://localhost:3000/authenticate",
-      headers
-    );
+    const response = await axios.get("http://localhost:3000/authenticate", {
+      headers: { Authorization: token },
+    });
 
     console.log("Token verification successful:", response.data);
+
     return { success: true };
   } catch (error) {
     console.error("Token verification failed:", error.message);
